4d18b0f3de4ac777f3cb182460a49d7236aebedc
jagadishchakma <jagadishchakma401@gmail.com>
2024-06-21 13:09:14 +0600

    Email wrong EMAIL

--- sonalibank/settings.py
+++ sonalibank/settings.py
@@ -156,7 +156,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env("Email")
+EMAIL_HOST_USER = env("EMAIL")
 EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
 
 
